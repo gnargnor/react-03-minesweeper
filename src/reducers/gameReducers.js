b2cdc392bd5440ecf896fedc8df6f96356bc1de7
Logan Kelly <logan.r.kelly@gmail.com>
2017-08-01 01:14:54 -0500

    game prep filling objects predictably

--- src/reducers/gameReducers.js
+++ src/reducers/gameReducers.js
@@ -7,7 +7,7 @@ const initialSettings = settings.easy;
 const configureDifficulty = (state, action) => {
     switch (action.difficulty) {
         case 'easy':
-            return settings.easy;
+            return {settings: settings.easy, game: prepareGame(settings.easy.rows, settings.easy.columns, settings.easy.mines)};
         case 'medium':
             return settings.medium;
         case 'hard':
@@ -17,12 +17,26 @@ const configureDifficulty = (state, action) => {
     }
 };
 
-const prepareGame = (rows, columns, mines) => {
-    const minefield = Array(rows).fill(Array(columns).fill({
-        hasMine: false,
-        hasBeenChecked: false,
-        minesNearby: 0,
-    }));
+const prepareGame = (numRows, numColumns, numMines) => {
+    const rows = Array(numRows).fill(null);
+    const minefield = rows.map((row, index) => {
+        const squares = Array(numColumns).fill({
+            row: index + 1,
+            column: 0,
+            hasMine: false,
+            hasBeenChecked: false,
+            minesNearby: 0
+        });
+        const thisRow = squares.map((square, index) => {
+            return Object.assign(
+                {},
+                square,
+                {column: index + 1}
+            );
+        });
+        return thisRow;
+    });
+    console.log('minefield row 3 column 5!', minefield[2][4]);
 };
 
 const gameReducer = (state = initialSettings, action) => {
