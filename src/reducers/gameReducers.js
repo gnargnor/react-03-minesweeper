54763971bb127bbb20c1298da23ebddd2e9ebcca
Logan Kelly <logan.r.kelly@gmail.com>
2017-08-09 21:53:52 -0500

    nearbyMines mapping properly

--- src/reducers/gameReducers.js
+++ src/reducers/gameReducers.js
@@ -2,44 +2,6 @@ import 'babel-polyfill';
 import * as settings from '../settings';
 import * as types from '../actions/actionTypes';
 
-const prepareGame = (numRows, numColumns, totalMines) => {
-    let rows = Array(numRows).fill(null);
-    let minefield = rows.map((row, index) => {
-        let squares = Array(numColumns).fill({
-            row: index,
-            column: 0,
-            id: 0,
-            hasMine: false,
-            hasBeenChecked: false,
-            minesNearby: 0
-        });
-        let thisRow = squares.map((square, index) => {
-            return Object.assign(
-                {},
-                square,
-                {column: index,
-                 id: index + (square.row * numRows) }
-            );
-        });
-        return thisRow;
-    });
-    let minesPlaced = 0;
-    for (let minesPlace = 0; minesPlaced < totalMines; minesPlaced++) {
-        let currentLocation = minefield[Math.floor(Math.random() * numRows)][Math.floor(Math.random() * numColumns)];
-        if (currentLocation.hasMine) {
-            minesPlaced--;
-        }
-        currentLocation.hasMine = true;
-    }
-    return minefield;
-};
-
-const initialSettings = Object.assign(
-    {},
-    settings.easy,
-    {minefield: prepareGame(settings.easy.rows, settings.easy.columns, settings.easy.mines)}
-);
-
 const configureDifficulty = (state, action) => {
     switch (action.difficulty) {
         case 'easy':
@@ -65,27 +27,7 @@ const configureDifficulty = (state, action) => {
     }
 };
 
-const handleClickedSquare = (state, action) => {
-    const clickedSquare = action.clickedSquare;
-    const minefield = state.minefield;
-    const rows = state.rows;
-    const columns = state.columns;
-    console.log(clickedSquare);
-    if (clickedSquare.hasBeenChecked  || clickedSquare === undefined) {
-        console.log(clickedSquare);
-        return state;
-    }
-    clickedSquare.hasBeenChecked = true;
-    if (clickedSquare.hasMine){
-        alert('BOOM');
-    } else {
-        clickedSquare.minesNearby = checkNearby(clickedSquare, minefield, rows, columns);  
-    }
-    console.log(minefield[clickedSquare.row][clickedSquare.column]);
-    return clickedSquare;
-};
-
-const checkNearby = (clickedSquare, minefield, rows, columns) => {
+const checkNearby = (minefield, rows, columns) => {
     let currentSquare;
     const validateRow = (clickedSquare, direction) => {
         try {
@@ -119,21 +61,91 @@ const checkNearby = (clickedSquare, minefield, rows, columns) => {
             return 0;
         }
     };
-    const nearbySquares = {
-        n: validateRow(clickedSquare, 'north'),
-        ne: validateRow(clickedSquare, 'northeast'),
-        e: validateRow(clickedSquare, 'east'),
-        se: validateRow(clickedSquare, 'southeast'),
-        s: validateRow(clickedSquare, 'south'),
-        sw: validateRow(clickedSquare, 'southwest'),
-        w: validateRow(clickedSquare, 'west'),
-        nw: validateRow(clickedSquare, 'northwest'),
-    };
-    let minesNearby = Object.values(nearbySquares);
-    return minesNearby.reduce((a,b) => {
-        return a + b;
-    });
+    
+    return minefield.map(row => {
+        return row.map(square => {
+            const nearbySquares = {
+                n: validateRow(square, 'north'),
+                ne: validateRow(square, 'northeast'),
+                e: validateRow(square, 'east'),
+                se: validateRow(square, 'southeast'),
+                s: validateRow(square, 'south'),
+                sw: validateRow(square, 'southwest'),
+                w: validateRow(square, 'west'),
+                nw: validateRow(square, 'northwest'),
+            };
+            let minesNearby = Object.values(nearbySquares);
+            return Object.assign(
+                {},
+                square,
+                {minesNearby: minesNearby.reduce((a,b) => a + b)}
+            );
+        });
+    }); 
 };
+
+const prepareGame = (numRows, numColumns, totalMines) => {
+    let rows = Array(numRows).fill(null);
+    let minefield = rows.map((row, index) => {
+        let squares = Array(numColumns).fill({
+            row: index,
+            column: 0,
+            id: 0,
+            hasMine: false,
+            hasBeenChecked: false,
+            minesNearby: 0
+        });
+        let thisRow = squares.map((square, index) => {
+            return Object.assign(
+                {},
+                square,
+                {column: index,
+                 id: index + (square.row * numRows) }
+            );
+        });
+        return thisRow;
+    });
+    let minesPlaced = 0;
+    for (let minesPlace = 0; minesPlaced < totalMines; minesPlaced++) {
+        let currentLocation = minefield[Math.floor(Math.random() * numRows)][Math.floor(Math.random() * numColumns)];
+        if (currentLocation.hasMine) {
+            minesPlaced--;
+        }
+        currentLocation.hasMine = true;
+    }
+    console.log( minefield );
+    let minefieldMap = checkNearby( minefield, numRows, numColumns);
+    console.log(minefieldMap);
+    return minefieldMap;
+};
+
+const initialSettings = Object.assign(
+    {},
+    settings.easy,
+    {minefield: prepareGame(settings.easy.rows, settings.easy.columns, settings.easy.mines)}
+);
+
+const handleClickedSquare = (state, action) => {
+    const clickedSquare = action.clickedSquare;
+    const minefield = state.minefield;
+    const rows = state.rows;
+    const columns = state.columns;
+    console.log(clickedSquare);
+    if (clickedSquare.hasBeenChecked  || clickedSquare === undefined) {
+        console.log(clickedSquare);
+        return state;
+    }
+    clickedSquare.hasBeenChecked = true;
+    if (clickedSquare.hasMine){
+        alert('BOOM');
+    } else {
+        clickedSquare.minesNearby = checkNearby(clickedSquare, minefield, rows, columns);  
+    }
+    console.log(minefield[clickedSquare.row][clickedSquare.column]);
+    return clickedSquare;
+};
+
+
 
 
 const gameReducer = (state = initialSettings, action) => {
