b312753fe92c863d599309d6656da02a0638e64a
Logan Kelly <logan.r.kelly@gmail.com>
2017-09-13 23:58:07 -0500

    mulligan

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -1,20 +1,20 @@
 export const easy = {
-    difficulty: 'easy',
-    rows: 10,
-    columns: 10,
-    mines: 10,
+  difficulty: 'easy',
+  rows: 10,
+  columns: 10,
+  mines: 10,
 };
 
 export const medium = {
-    difficulty: 'medium',
-    rows: 16,
-    columns: 16,
-    mines: 40
+  difficulty: 'medium',
+  rows: 16,
+  columns: 16,
+  mines: 40
 };
 
 export const hard = {
-    difficulty: 'hard',
-    rows: 16,
-    columns: 40,
-    mines: 99
+  difficulty: 'hard',
+  rows: 16,
+  columns: 40,
+  mines: 99
 };
